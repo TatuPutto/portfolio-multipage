28e38d0d71c1d1613254c52c60538ad33ab80b44
Tatu Putto <tatu.putto@hotmail.com>
2017-11-28 15:42:00 +0200

    Express setup

---
+++ src/server.js
@@ -0,0 +1,22 @@
+var express    = require('express');
+var app        = express();
+var path       = require('path');
+var port       = process.env.PORT || 8080;
+var publicPath = path.join(__dirname, '../public');
+
+app.set('publicPath', publicPath);
+app.use(express.static(app.get('publicPath')));
+
+// server landing page
+app.get('/', function (req, res) {
+    res.sendFile(path.join(publicPath, 'home.html'));
+});
+
+// server projects page
+app.get('/projects', function (req, res) {
+    res.sendFile(path.join(publicPath, 'projects.html'));
+});
+
+app.listen(port, function () {
+    console.log('Listening at: ' + port);
+});
